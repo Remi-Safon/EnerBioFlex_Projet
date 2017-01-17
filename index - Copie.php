d6cc4527783762294a66e981b0d486753060969e
Stephane <stephane.boucaud@outlook.fr>
2017-01-17 11:28:12 +0100

    test

---
+++ index - Copie.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8">
+		<title>Plateforme d'échange</title>
+		<link rel="shortcut icon" href="img/logo.png">
+		<link rel="stylesheet" href="css/Enerbioflex.css"/>
+		<link rel="stylesheet" href="css/annonce.css"/>
+		
+		<!--[if lt IE 9]>
+			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		
+	</head>
+	<body>
+	
+	<?php
+		require 'php/annonce.php';
+	?>
+	
+	<?php
+		write_article(00001,"Annonce",125, "dferfzeffzef  efz efefefezfef fezfze fefzefe fezfzefzefze zef zefze zefze fezzefezrfezefzefefzefze", "25 rue de paris", "");
+	?>
+	
+	</body>
+</html>
