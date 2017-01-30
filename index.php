ab0c0191cfc29a90f0df207bef7facbb8b667ae2
onaprimonblase <remisafon@gmail.com>
2017-01-30 23:42:40 +0100

    recherhe d'article
    Avancer de la page de recherhe d'article

--- index.php
+++ index.php
@@ -16,23 +16,66 @@
 		
 		// CONNECTION BDD
 			$bdd = connect_database('localhost', "bdd_enerbioflex", 'utf8', 'root', '');
+		
+		
+		
+		
+		if(isset($_GET['type_ressource']) && isset($_GET['departement'])){
 			
-		// PREPARATION DE LA REQUETE
+			$laRessource=$_GET['type_ressource'];
+			$laRegion=$_GET['departement'];
+			
+			
+			
+			if(isset($_GET['nomArticle'])){
+				$leArticle=$_GET['nomArticle'];
+				// PREPARATION DE LA REQUETE
+				$req=$bdd->prepare('SELECT id_article, titre, prix, description, voie, photo, region, departement, ville FROM article 
+				JOIN region USING(id_region)
+				JOIN departement USING(id_departement)
+				JOIN ville USING(id_ville)
+				JOIN ressource USING(id_ressource)
+				WHERE ressource= ? AND region= ? AND titre LIKE ?;');
+				
+				// EXECUTION DE LA REQUETE
+				$req->execute(array($laRessource, $laRegion, "%$leArticle%"));
+			}
+			else{
+				// PREPARATION DE LA REQUETE
+				$req=$bdd->prepare('SELECT id_article, titre, prix, description, voie, photo, region, departement, ville FROM article 
+				JOIN region USING(id_region)
+				JOIN departement USING(id_departement)
+				JOIN ville USING(id_ville)
+				JOIN ressource USING(id_ressource)
+				WHERE ressource= ? AND region= ?;');
+				
+				// EXECUTION DE LA REQUETE
+				$req->execute(array($laRessource, $laRegion));
+			}
+		}
+	
+		else{ // SI IL N'Y A PAS LES ARGUMENTS DE RECHERCHE
+		
+			// PREPARATION DE LA REQUETE
 			$req=$bdd->prepare('SELECT id_article, titre, prix, description, voie, photo, region, departement, ville FROM article 
 			JOIN region USING(id_region)
 			JOIN departement USING(id_departement)
 			JOIN ville USING(id_ville)
 			WHERE 1;');
-		
-		// EXECUTION DE LA REQUETE
+			
+			// EXECUTION DE LA REQUETE
 			$req->execute();
+
+		}
+
+		
 		
 			while($resultat=$req->fetch()){
 				write_article($resultat['id_article'],$resultat['titre'],$resultat['prix'], $resultat['description'], $resultat['region'], $resultat['departement'], $resultat['ville'], $resultat['photo']);
 			}
 
 	
-		?>
+?>
 			
 
 	
--- php/recherche.php
+++ php/recherche.php
@@ -1,42 +1,59 @@
-
-
 <?php 
-	
+	// CONNECTION DATABASE
 	$bdd = connect_database('localhost', 'bdd_enerbioflex', 'utf8', 'root', '');
-
+	
 	// PREPARATION DE LA REQUETE
 	$req=$bdd->prepare('SELECT ressource FROM ressource WHERE 1');
-			
+				
 	// EXECUTION DE LA REQUETE
 	$req->execute();
-	
+		
 	// PREPARATION DE LA REQUETE
 	$req2=$bdd->prepare('SELECT region FROM region WHERE 1');
-			
+				
 	// EXECUTION DE LA REQUETE
 	$req2->execute();
-	
-?>
-
-<form action="ajouter.php" method="get">
 		
+	
 
-	<p><input type="text" name="nom" value="Recherche"/></p>
-	<SELECT name="type_ressource"  size="1">
-<?php	
-	while($resultat_ressource=$req->fetch()){
+echo '<form action="'.$_SERVER['PHP_SELF'].'" method="get">'; // FORMULAIRE
+		
+	echo '<label> Recherche </label><p><input type="text" name="nomArticle"/></p>';
+	echo '<SELECT name="type_ressource"  size="1">';
+	
+	
+	if(isset($laRessource)){ // SI LE NOM DE LA RESSOURCE A DEJA ETE SELECTIONNER UNE PREMIERE FOIS
+		
+		
+		while($resultat_ressource=$req->fetch()){  // ALIMENTATION DES LISTE DEROULANTE AVEC LES RESSOURCES DE LA BASE
 		echo '<OPTION>'.$resultat_ressource['ressource']; 
+		}
 	}
-?>
-	</SELECT>
-	<SELECT name="Département" size="1">
-<?php	
-	while($resultat_region=$req2->fetch()){
-		echo '<OPTION>'.$resultat_region['region']; 
+	else{
+		while($resultat_ressource=$req->fetch()){  // ALIMENTATION DES LISTE DEROULANTE AVEC LES RESSOURCES DE LA BASE
+		echo '<OPTION>'.$resultat_ressource['ressource']; 
+		}
 	}
-?>
-	</SELECT>
+	
 
+	echo '</SELECT>';
+	echo '<SELECT name="departement" size="1">';
+
+	
+	if(isset($laRegion)){ // SI LE NOM DE LA REGION A DEJA ETE SELECTIONNER UNE PREMIERE FOIS
+		while($resultat_region=$req2->fetch()){  // ALIMENTATION DES LISTE DEROULANTE AVEC LES REGIONS DE LA BASE
+			
+			if($laRegion=$resultat_region['region']){echo '<OPTION selected="selected">'. $resultat_region['region'];}
+			else{echo '<OPTION>'. $resultat_region['region'];}
+		}	
+	}
+	else{
+		while($resultat_region=$req2->fetch()){  // ALIMENTATION DES LISTE DEROULANTE AVEC LES REGIONS DE LA BASE
+			echo '<OPTION>'.$resultat_region['region'];
+		}
+	}
+?>
+	</SELECT>
 	
 	<p>  <input type="submit" name="Ajouter" value="Rechercher"/></p>
 </form>
