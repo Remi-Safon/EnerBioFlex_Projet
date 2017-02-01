64bff495eb1fc9b2c880889a79db392bea63d34e
Stephane <stephane.boucaud@outlook.fr>
2017-02-01 19:14:18 +0100

    Modification script connexion DDB

--- php/connection_BDD.php
+++ php/connection_BDD.php
@@ -1,15 +1,21 @@
-<?php 
-
-function connect_database($domain, $dbname, $charset, $root, $password){
+<?php
 
 	// CONNECTION BDD
 	
+	$sgbd_type = 'mysql';
+	$domain = 'localhost';
+	$dbname = 'bdd_enerbioflex';
+	$charset = 'utf8';
+	$root = 'root';
+	$password = '';
+
 	try{
-		$bdd = new PDO('mysql:host='.$domain.';dbname='.$dbname.';charset='.$charset,$root, $password);
-		return $bdd;
+		$bdd = new PDO($sgbd_type.':host='.$domain.';dbname='.$dbname.';charset='.$charset,$root, $password);
+		$bdd->query('SET NAMES utf8');
+		$bdd->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 	}
-	catch (Exception $e){
-		die('Erreur : ' . $e->getMessage());		
+	catch (PDOException $e){
+		die('<p>Erreur connection BDD: '.$e->getMessage().'</p>');
 	}
-}
-?>	
+	
+?>
