32de139c53ef0f90c5963c1b29b9b46126b4e1cc
onaprimonblase <remisafon@gmail.com>
2017-03-06 20:22:39 +0100

    maj envoyerMail

--- php/functions.inc.php
+++ php/functions.inc.php
@@ -362,54 +362,7 @@ function head_html( $title, $icon, $css_files , $add){ // add contient d'autres 
 	
 	
 	
-	function envoieMail($destinataire, $objet, $message_txt, $message_html){
-	
-	
 
-		if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) // On filtre les serveurs qui rencontrent des bogues.
-		{
-			$passage_ligne = "\r\n";
-		}
-		else
-		{
-			$passage_ligne = "\n";
-		}
-
-
-		//=====Création de la boundary
-		$boundary = "-----=".md5(rand());
-		//==========
-
-		 
-		//=====Création du header de l'e-mail.
-		$header = "From: \"EnerBioFlex\"<enerbioflex@ovh.net>".$passage_ligne;
-		$header.= "Reply-to: \"EnerBioFlex\" <enerbioflex@ovh.net>".$passage_ligne;
-		$header.= "MIME-Version: 1.0".$passage_ligne;
-		$header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
-		//==========
-		 
-		//=====Création du message.
-		$message = $passage_ligne."--".$boundary.$passage_ligne;
-		//=====Ajout du message au format texte.
-		$message.= "Content-Type: text/plain; charset=\"ISO-8859-1\"".$passage_ligne;
-		$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
-		$message.= $passage_ligne.$message_txt.$passage_ligne;
-		//==========
-		$message.= $passage_ligne."--".$boundary.$passage_ligne;
-		//=====Ajout du message au format HTML
-		$message.= "Content-Type: text/html; charset=\"ISO-8859-1\"".$passage_ligne;
-		$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
-		$message.= $passage_ligne.$message_html.$passage_ligne;
-		//==========
-		$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
-		$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
-		//==========
-		 
-		//=====Envoi de l'e-mail.
-		mail($destinataire,$objet,$message,$header);
-		//==========
-
-}
 
 
 
